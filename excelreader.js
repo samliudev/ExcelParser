1251532c8b9f953c3b11f2abbb532208d52dc3ad
Samuel Liu <samliu14@gmail.com>
2022-12-28 00:30:04 -0500

    cleaning up helper functions

--- excelreader.js
+++ excelreader.js
@@ -1,12 +1,10 @@
 const reader = require('xlsx')
-const file = reader.readFile('./Input/input.xlsx')
+const file = reader.readFile('./Input/input.csv')
 
-let excelData = [] // contains all the information from the excel sheet
-
-
+let excelData = []      // contains all the information from the excel sheet
 const sheets = file.SheetNames
 
-const readExcelFile = () => {
+const readExcelFile = () => {            // helper function to parse through excel input
     for (let i = 0; i < sheets.length; i++) {
         const temp = reader.utils.sheet_to_json(
             file.Sheets[file.SheetNames[i]], {header: 1, raw: false})
@@ -16,13 +14,11 @@ const readExcelFile = () => {
     }
 }
 
-let rows = []
-const output = reader.utils.json_to_sheet(rows)
-readExcelFile()
+let cache = {}
 
-const cacheExcelData = () => {
-    let cache = {}
-    for (const data of excelData) {                    // caching all the excel data by customer Id
+const cacheExcelData = () => {            // helper function to cache the excel data by customer Id
+    readExcelFile()
+    for (const data of excelData) { 
         const customer = data[0]
         const month = data[1]
         const amount = data[2]
@@ -32,23 +28,29 @@ const cacheExcelData = () => {
             cache[customer].push([month, amount])
         }
     }
+}
+
+const createOutput = () => {        // main function to take cache and output an excel sheet
+    cacheExcelData()
     let monthlyResults = []
     for (let customerId in cache) { 
-        cache[customerId].sort((a,b) => new Date(a[0]) - new Date(b[0])) // sorts dates in cache by date
-
-        let monthlyData = processCache(customerId, cache)
+        cache[customerId].sort((a,b) => {       // sorts each customers accounts by date
+            return new Date(a[0])- new Date(b[0]) || b[1] - a[1]   // if there are multiple transactions on the same day, apply credits first
+        }) 
+        let monthlyData = calculateMonthlies(customerId, cache)
         for (let data in monthlyData) {
             monthlyResults.push([customerId, data, ...monthlyData[data]])
         }
     }
-    console.log(monthlyResults)
-    const worksheet = reader.utils.json_to_sheet(monthlyResults, {skipHeader: true})
+    const worksheet = reader.utils.json_to_sheet(monthlyResults, {skipHeader: true, dateNF: 'mm"."yyyy'})
     const workbook = reader.utils.book_new()
+    const csv = reader.utils.sheet_to_csv(worksheet, {skipHeader: true})
+    console.log(csv)
     reader.utils.book_append_sheet(workbook, worksheet, "Monthly Balances")
-    reader.writeFile(workbook, "AccountTransactions.xlsx")
+    reader.writeFile(workbook, "AccountTransactions.csv", {cellDates: false})
 }
-// 11/2022 : min, max, current
-const processCache = (customerId, cache) => {       // keeping track of max and min and final per month
+
+const calculateMonthlies = (customerId, cache) => {       // helper function to keep track of max and min and final per month
     const accounts = cache[customerId]
     let monthlyData = {}
     let current = min = max = 0
@@ -57,18 +59,17 @@ const processCache = (customerId, cache) => {       // keeping track of max and 
         let year = new Date(accounts[i][0]).getFullYear()
         const time = month + "/" + year
         if (monthlyData[time] === undefined) { 
-            current = Number(accounts[i][1])
-            max = min = current                           // new month/year, so reset min and max
+            isNaN(Number(accounts[i][1])) ? current = 0 : current = Number(accounts[i][1])  // if the amount is NaN, we'll treat it as "0"
+            max = min = current                             // new month/year, so reset min and max
             monthlyData[time] = [min, max, current]
-        } else {                                    // update existing entry
-            current += Number(accounts[i][1])
+        } else {                                         
+            isNaN(Number(accounts[i][1])) ? current += 0 : current += Number(accounts[i][1])
             if (current > max) max = current
             if (current < min) min = current
             monthlyData[time] = [min, max, current]
         }
-        
     }
     return monthlyData
 }   
 
-cacheExcelData()
+createOutput()
